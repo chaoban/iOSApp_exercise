81ffa0e0d972bbdf788ab17af30a9ca9671fdb6d
Wang Chao Pang <mm579016@ms17.hinet.net>
2021-07-30 20:38:21 +0800

    add ch8 simple table

---
+++ ios13_程式開發心法/SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 王肇邦 on 2021/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
