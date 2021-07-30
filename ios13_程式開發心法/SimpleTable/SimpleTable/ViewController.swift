cb2b1c49107ce584619385f21b8a1c7b0c515a61
Wang Chao Pang <mm579016@ms17.hinet.net>
2021-07-30 21:25:21 +0800

    add readme file

--- ios13_程式開發心法/SimpleTable/SimpleTable/ViewController.swift
+++ ios13_程式開發心法/SimpleTable/SimpleTable/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var restaurantNames = ["王肇邦", "陳玟霖", "王筱喬", "王宥棋", "陳映汝", "陳亭蓁", "陳柏穎", "楊媽媽", "老謝", "老劉", "雙胞胎大女兒", "雙胞胎小女兒", "豬小弟"]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return restaurantNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cellIdentifier = "datacell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        return cell
     }
     
 
